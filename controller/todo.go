0ee9ccafc17fb57a67c5589b49a8a9e96a5f9473
enesce <selimenesc@gmail.com>
2023-01-10 17:06:22 +0300

    middleware jwt ready

--- controller/todo.go
+++ controller/todo.go
@@ -1,10 +1,69 @@
 package controller
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/enescedev/gotodo/config"
 	"github.com/enescedev/gotodo/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
+
+/*
+func Jwt(c *gin.Context) {
+
+		var u User
+		if err := c.ShouldBindJSON(&u); err != nil {
+		   c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		   return
+		}
+		//compare the user from the request, with the one we defined:
+		if user.Username != u.Username || user.Password != u.Password {
+		   c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		   return
+		}
+		token, err := CreateToken(user.ID)
+		if err != nil {
+		   c.JSON(http.StatusUnprocessableEntity, err.Error())
+		   return
+		}
+		c.JSON(http.StatusOK, token)
+	  }
+*/
+type User struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+func GetJWT(c *gin.Context) {
+
+	var user User
+	user.Id = 1
+	user.Name = "Bulut Bilisimciler"
+	// github.com/golang-jwt/jwt
+	// TODO: Token üret...
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": user,
+		"exp":  time.Now().Add(time.Hour).Unix(),
+		"sub":  user.Id,
+	})
+	key := config.Key
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(key)
+
+	fmt.Println(tokenString, err)
+	// TODO: Token'ı kullanıcıya gönder...
+
+	// middlewares/middleware_jwt.go (Yazılacak)
+	c.JSON(200, gin.H{
+
+		"token": (tokenString),
+	})
+}
 
 func GetToDo(c *gin.Context) {
 	todos := []models.Todo{}
@@ -13,6 +72,9 @@ func GetToDo(c *gin.Context) {
 }
 
 func CreateToDo(c *gin.Context) {
+	user, _ := c.Get("user")
+	usermodel, _ := user.(User)
+	log.Println("user=", usermodel)
 	var todos models.Todo
 	c.BindJSON(&todos)
 	config.DB.Create(&todos)
--- routes/todo.go
+++ routes/todo.go
@@ -2,12 +2,15 @@ package routes
 
 import (
 	"github.com/enescedev/gotodo/controller"
+	"github.com/enescedev/gotodo/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func ToDoRoute(router *gin.Engine) {
+	router.Use(middleware.JwtRequiredMiddleware)
+	router.GET("/generate-jwt", controller.GetJWT)
 	router.GET("/", controller.GetToDo)
-	router.POST("/", controller.CreateToDo)
+	router.POST("/createtodo", middleware.JwtRequiredMiddleware, controller.CreateToDo)
 	router.DELETE("/:id", controller.DeleteToDo)
 	router.PUT("/:id", controller.UpdateToDo)
 }
